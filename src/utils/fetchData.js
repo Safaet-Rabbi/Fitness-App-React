75d03a5ba8331ece624c5b2bfdc16ba9f6cfad9a
Safaet-Rabbi <86919997+Safaet-Rabbi@users.noreply.github.com>
2024-08-04 18:32:48 +0600

    Add files via upload

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': '7c7f8c7842mshb1fadc4257d05a7p11eddfjsnd899fdceb28e',
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': '7c7f8c7842mshb1fadc4257d05a7p11eddfjsnd899fdceb28e',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
